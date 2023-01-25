f596a9a1250f74281799395d4076d5e6f6146f83
Francois Liot <francois.liot@epfl.ch>
2023-01-25 21:39:07 +0100

    Updated button label and tooltip message

--- invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/landing_page/RecordManagement.js
+++ invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/landing_page/RecordManagement.js
@@ -32,7 +32,10 @@ export const RecordManagement = ({
 
         <Popup
           trigger={<Icon className="ml-0" name="info circle" style={{"line-height": "normal", "padding-top": "5px", "padding-bottom": "0px", "padding-left": "0px", "padding-right": "0px", "margin": "0px"}}/>}
-          content={'Click "Edit" to modify the metadata of the published record without creating a new version. Click "New version" to update the list of attached files. Click "Share" to let other users of your choice alter the record.'}
+          content={
+          'Click "Edit" to modify the record\'s metadata (title, authors, etc) without creating a new version. ' +
+            'Click "New version" if you wish to add or delete files or simply want to keep track of your changes. ' +
+            'Click "collaborate" to collaborate with others on that record.'}
         />
 
       {permissions.can_edit && !isDraft && (
